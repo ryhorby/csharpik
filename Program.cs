990f271ab17aa04a6f2259bb3788c591006d0491
ryhor.by <ryhor.by@outlook.com>
2022-03-27 23:14:30 +0300

    add exceprion handler in developer mode

--- Program.cs
+++ Program.cs
@@ -19,10 +19,12 @@ builder.Services.AddScoped<BookService>();
 
 var app = builder.Build();
 
-
-app.UseExceptionHandler("/Home/Error");
-// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-app.UseHsts();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 
 
 app.UseHttpsRedirection();
