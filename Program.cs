5e427df680fca4354f949ddb47581a623c30dc93
ryhor.by <ryhor.by@outlook.com>
2022-03-28 16:02:55 +0300

    chenge environment

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddScoped<BookService>();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error/{0}");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
